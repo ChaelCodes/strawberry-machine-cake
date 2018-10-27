d35174ab0d9e51d896081cbe047a0fefe4e42c45
Rachael <rachaellw5.1@gmail.com>
2018-10-27 15:10:21 +0000

    Whitelist scenario_id

--- app/controllers/pixes_controller.rb
+++ app/controllers/pixes_controller.rb
@@ -69,6 +69,6 @@ class PixesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def pix_params
-      params.require(:pix).permit(:character_id, :msg)
+      params.require(:pix).permit(:character_id, :scenario_id, :msg)
     end
 end
