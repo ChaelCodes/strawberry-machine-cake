c45437416ae53898d30f6dc553aa140feec47028
Rachael <rachaellw5.1@gmail.com>
2018-11-16 15:16:56 +0000

    Page through character iterations on the character show page.

--- app/models/character.rb
+++ app/models/character.rb
@@ -2,7 +2,7 @@ class Character < ApplicationRecord
   has_many :pixes
   has_many :likes
   belongs_to :previous_character, class_name: 'Character', optional: true
-  has_one :next_character, class_name: 'Character', optional: true, foreign_key: :previous_character_id
+  has_one :next_character, class_name: 'Character', foreign_key: :previous_character_id
 
   has_one_attached :avatar
 
