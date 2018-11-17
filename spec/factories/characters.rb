674abb94c5138277caa0a797c378758e63ffc046
Rachael <rachaellw5.1@gmail.com>
2018-11-17 00:14:28 +0000

    Set up Factories - factory for character

---
+++ spec/factories/characters.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :character do
+    display_name { '7ara' }
+    state { 'active' }
+  end
+end
--- spec/models/character_spec.rb
+++ spec/models/character_spec.rb
@@ -1,10 +1,23 @@
 describe Character do
+  let(:character) { build :character }
+
+  describe '#new' do
+    subject { character }
+
+    it { is_expected.to be_valid }
+    it 'stores attributes' do
+      expect(subject.display_name).to eq '7ara'
+    end
+  end
+
   describe '#pixes' do
-    let(:character) { Character.new }
     subject { character.pixes }
 
     it 'returns nothing' do
       is_expected.to be_empty
     end
+  end
+
+  describe '#deactivate' do
   end
 end
