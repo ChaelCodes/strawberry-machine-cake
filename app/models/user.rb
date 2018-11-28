19a47f29e383cbbe175c0d02bfea89a92cdb0b3b
Rachael <rachaellw5.1@gmail.com>
2018-11-28 23:58:41 +0000

    Code Climate Fixes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
+# frozen_string_literal: true
+
 # User - login, session tracking, authentication, etc
 # Vanilla Devise
-
-# frozen_string_literal: true
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
